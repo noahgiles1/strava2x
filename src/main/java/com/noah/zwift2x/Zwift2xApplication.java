98ab3de01bf683358be58456f09867391ce62a91
noahgiles1 <mrnoahgiles@gmail.com>
2025-04-22 21:53:16 +0100

    Initialize Spring Boot project with Maven wrappers

---
+++ src/main/java/com/noah/zwift2x/Zwift2xApplication.java
@@ -0,0 +1,13 @@
+package com.noah.zwift2x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zwift2xApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Zwift2xApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/noah/zwift2x/Zwift2xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noah.zwift2x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zwift2xApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
